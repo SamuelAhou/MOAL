2b04f7290c11f6814a5e8d0feb4016deeebec56b
Samuel Ahou <samuelahou@hotmail.com>
2021-07-31 18:36:38 +0200

    makefile update

--- Makefile
+++ Makefile
@@ -2,16 +2,10 @@ CC = gcc
 CFLAGS = -g
 LIB = -lm
 
-SRC = $(wildcard *.c)
+SRC = $(wildcard ./src/*.c)
 OBJ = $(wildcard *.o)
 
 all : main
 
-main : LAF.o UF.o
-	$(CC) LAF.o UF.o -o main $(LIB)
-
-LAF.o : ./src/LAF.c
-	gcc ./src/LAF.c -c $(LIB)
-
-UF.o : ./src/UF.c
-	gcc ./src/UF.c -c $(LIB)
+main : 
+	$(CC) $(SRC) main.c -o main $(LIB)
--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include "./src/LAF.c"
-#include "./src/UF.c"
-#include "./src/Tools.c"
+#include "./headers/LAF.h"
+#include "./headers/UF.h"
+#include "./headers/Tools.h"
 
 int main(int argc, char *argv[])
 {
     initBuffer();
 
-    
+    float array[9] = {1,2,3,4,5,6,7,8,9};
+    float** A = matrixFromArray(3,3,array);
+    displayMatrix(3,3,A);
 
     freeAll();
     return 0;
